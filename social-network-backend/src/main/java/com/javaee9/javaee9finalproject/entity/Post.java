22ce0f7af82bae55e9120149257dccc012d98ba3
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-03-27 09:51:33 +0300

    [change] - repository for Post entity + some enhancement to entity

--- social-network-backend/src/main/java/com/javaee9/javaee9finalproject/entity/Post.java
+++ social-network-backend/src/main/java/com/javaee9/javaee9finalproject/entity/Post.java
@@ -5,16 +5,34 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.Clock;
 import java.time.ZonedDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id; // it's a primary key
     String header;
     String content;
     String author;
     ZonedDateTime creationTimeStamp;
     ZonedDateTime updateTimeStamp; // in case we need to update/edit post
+
+    @PrePersist
+    private void setInitialTimestamp() {
+        ZonedDateTime timestamp = ZonedDateTime.now(Clock.systemUTC());
+        creationTimeStamp = timestamp;
+        updateTimeStamp = timestamp;
+    }
+
+    @PreUpdate
+    private void setUpdateTimestamp() {
+        updateTimeStamp = ZonedDateTime.now(Clock.systemUTC());
+    }
 }
